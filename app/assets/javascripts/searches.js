ef2ef8f7bbcfb9293b9af807b9a23cecabe40386
Calvin Wang <calvinkw1@gmail.com>
2015-03-29 18:59:28 -0700

    search result data now fully captured and appended

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -8,23 +8,21 @@ $(document).ready(function() {
       childCount = 0,
       maxStops = 0,
       preferredCabin = "COACH",
-      permittedCarrier = "",
-      prohibitedCarrier = "",
-      solutions = 1;
+      permittedCarrier = [""],
+      prohibitedCarrier = [""];
 
   $("#search-input").submit(function(e) {
     e.preventDefault();
-    origin = $("#origin").val();
-    destination = $("#destination").val();
+    origin = $("#origin").val().toUpperCase();
+    destination = $("#destination").val().toUpperCase();
     departDate = $("#departDate").val();
     returnDate = $("returnDate").val();
     adultCount = parseInt($("#adultCount").val(), 10);
     childCount = parseInt($("#childCount").val(), 10);
     maxStops = parseInt($("#maxStops").val(), 10);
-    preferredCabin = $("#preferredCabin").val();
-    permittedCarrier = $("#permittedCarrier").val();
+    preferredCabin = $("#preferredCabin").val().toUpperCase();
+    permittedCarrier = $("#permittedCarrier").val().toUpperCase().split(",");
     prohibitedCarrier = prohibitedCarrier;
-    solutions = parseInt($("#solutions").val(), 10);
     startAPICall();
   });
 
@@ -50,22 +48,18 @@ $(document).ready(function() {
             "preferredCabin": preferredCabin,
             "permittedDepartureTime": {
               "kind": "qpxexpress#timeOfDayRange",
-              "earliestTime": "00:00",
-              "latestTime": "11:59"
+              "earliestTime": "",
+              "latestTime": ""
             },
-            "permittedCarrier": [
-              permittedCarrier
-            ],
+            "permittedCarrier": permittedCarrier,
             "alliance": "",
-            "prohibitedCarrier": [
-              prohibitedCarrier
-            ]
+            "prohibitedCarrier": prohibitedCarrier
           }
         ],
         "maxPrice": "USD3000.00",
         // "saleCountry": "US",
         "refundable": false,
-        "solutions": solutions
+        "solutions": 500
       }
     };
     $.ajax({
@@ -75,8 +69,10 @@ $(document).ready(function() {
       // contentType: "application/json; charset=utf-8",
       data: {qpxData: JSON.stringify(reqBody)},
       success: function(data) {
-        console.log(data.results.trips.tripOption);
+        $(".results").show();
+        console.log(data);
         results = data.results.trips.tripOption;
+        flightData = data.results.trips.data;
           $("tbody").empty();
         for (var i = 0; i < results.length; i++) {
           $("tbody").append("<tr id='result-row" + i + "'>");
@@ -85,6 +81,8 @@ $(document).ready(function() {
           $("#result-row" + i).append("<td>" + results[i].slice[0].segment[0].leg[0].departureTime + "</td>");
           $("#result-row" + i).append("<td>" + results[i].slice[0].segment[0].leg[0].arrivalTime + "</td>");
           $("#result-row" + i).append("<td>" + results[i].pricing[0].passengers.adultCount + "</td>");
+          $("#result-row" + i).append("<td>" + results[i].slice[0].segment[0].cabin + "</td>");
+          $("#result-row" + i).append("<td>" + results[i].slice[0].segment[0].flight.carrier + " " + results[i].slice[0].segment[0].flight.number + "</td>");
           $("#result-row" + i).append("<td>" + results[i].slice[0].segment[0].leg[0].aircraft + "</td>");
           $("#result-row" + i).append("<td>" + results[i].pricing[0].baseFareTotal + "</td>");
           $("#result-row" + i).append("<td>" + results[i].pricing[0].saleTaxTotal + "</td>");
