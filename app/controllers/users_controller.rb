33414ad6b6659ff5e00a234af1e174b95e1f7669
Nick Marazzo <designmarz@gmail.com>
2015-03-31 08:02:22 -0700

    changed welcome email delivery to _now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      UserMailer.welcome_email(@user).deliver_later
+      UserMailer.welcome_email(@user).deliver_now
       session[:user_id] = @user.id
       flash[:success] = "User was successfully created."
       redirect_to root_path
