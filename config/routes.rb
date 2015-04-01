2258be9544ba5a658d0d2d725bff2be59fc25a89
Nick Marazzo <designmarz@gmail.com>
2015-04-01 16:07:22 -0700

    changed routes back to #new, checking if schedueled tasks execute properly

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get 'searches/index'
 
                       #will need to change this back to searches#new 
-  post '/search', to: 'searches#alertSearch', as: 'new_search'
+  post '/search', to: 'searches#new', as: 'new_search'
 
   post '/save', to: 'searches#save', as: 'save_search'
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,53 +1,13 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-every 1.day, :at => "10:25pm" do
-  runner "UserMailer.admin_email.deliver_now"
-end
-
-# FOR TESTING ONLY CHANGE TIME ONCE CONFIRMED
-# every 1.minutes do
-#   runner "SearchesController.alertSearch"
-# end
-
-every 1.day, :at => "10:25am" do
+every 1.day, :at => "4:15pm" do
   runner "UserMailer.nightly_update.deliver_now"
 end
 
-every 1.hour do
-  runner "UserMailer.alert_email.deliver_now"
+every 1.day, :at => "4:10pm" do
+  runner "SearchesController.alertSearch"
 end
 
-# FOR TESTING ONLY REMOVE ONCE CONFIRMED
-# every 1.minutes do #change time to 15 minutes
-#  command "whenever -i" #need to get thsi working
-#  puts "ran whenever update"
-#   # runner "UserMailer.admin_email.deliver_now" #remove this
-# end
 
-# every 1.minutes do #change time to 15 minutes
-#  command "/usr/bin/whenever -i" #need to get thsi working
-#  puts "ran whenever update"
-#   # runner "UserMailer.admin_email.deliver_now" #remove this
-# end
 
 every 1.minutes do #change time to 15 minutes
  command "cd /Users/NickMarazzo/Desktop/final-project-specs/ && whenever -i" #need to get thsi working
